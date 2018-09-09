301e34ce8b9d997094eaeecc7f7cb5f3ae606cb7
Dan Osborne <dan.osborne@rocketmail.com>
2018-09-09 22:07:24 +0100

    core set-up

---
+++ services/twitter.js
@@ -0,0 +1,29 @@
+const Twit = require('twit');
+
+const twitterService = (req, res, next) => {
+
+  const twitterFeed = new Twit({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    timeout_ms: 60*1000,
+  });
+  
+  const params = {
+    screen_name: 'JimmySynthetic',
+    exclude_replies: true,
+    include_rts: false,
+    count: 20
+  };
+
+  twitterFeed.get('statuses/user_timeline', params, (error, tweets, response) => {
+    if (!error) {
+      res.status(200).json(tweets);
+    } else {
+      res.status(500).send(error);
+    };
+  });
+}
+
+module.exports = twitterService;
